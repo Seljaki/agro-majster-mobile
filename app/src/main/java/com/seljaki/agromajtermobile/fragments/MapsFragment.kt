24904ef9cffd4e329cfec827834e443ab6774a54
mzHap <mojcazamuda12@gmail.com>
2025-01-17 08:07:25 +0100

    added dropdown to filter weather added based on time

--- app/src/main/java/com/seljaki/agromajtermobile/fragments/MapsFragment.kt
+++ app/src/main/java/com/seljaki/agromajtermobile/fragments/MapsFragment.kt
@@ -2,29 +2,33 @@ package com.seljaki.agromajtermobile.fragments
 
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.core.content.res.ResourcesCompat.getDrawable
+import androidx.fragment.app.Fragment
 import com.seljaki.agromajtermobile.MyApplication
 import com.seljaki.agromajtermobile.R
 import com.seljaki.agromajtermobile.databinding.FragmentMapsBinding
-import com.seljaki.lib.Blockchain
 import org.osmdroid.config.Configuration
-import org.osmdroid.library.BuildConfig
-import org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow
-import java.util.UUID
+import java.time.Instant
+import java.util.Date
+import kotlin.math.log
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), AdapterView.OnItemSelectedListener {
     lateinit var binding: FragmentMapsBinding
     lateinit var app: MyApplication
+    var selectedTimeFrame: Long = 0
+    val points: MutableList<GeoPoint> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +47,7 @@ class MapsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        showDropdown()
         setupMap()
         showMarkers()
     }
@@ -60,16 +65,15 @@ class MapsFragment : Fragment() {
     }
 
     private fun showMarkers() {
+        points.clear()
         val map = binding.mapView
-        val points: MutableList<GeoPoint> = mutableListOf()
         val blocks = app.blockchain.blocks;
         map.overlays.clear()
-
         for(block in blocks) {
-            if(block.index == 0) continue
+            Log.d("MapsFragment", "Processing block: index=${block.index}, timestamp=${block.timestamp}, selectedTimeFrame=$selectedTimeFrame")
+            if(block.index == 0 || (block.timestamp < selectedTimeFrame && selectedTimeFrame != 0L)) continue
             val geoPoint = GeoPoint(block.data.longitude, block.data.latitude)
             points.add(geoPoint)
-
             val marker = Marker(map)
             marker.position = geoPoint
             marker.title = "Block Info"
@@ -91,6 +95,52 @@ class MapsFragment : Fragment() {
             map.setExpectedCenter(points.get(0))
             //map.zoomToBoundingBox(boundingBox, true) // Adjusts zoom level to fit all markers
         }
+        map.invalidate()
+    }
+
+    private fun showDropdown(){
+        val spinner: Spinner = binding.spinner
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.dates_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val selectedItem = parent.getItemAtPosition(pos).toString()
+        val date = Instant.now().epochSecond
+        val oneDayInMillis: Long = 24L * 60L * 60L
+        when (selectedItem) {
+            "Last day" -> {
+                selectedTimeFrame = date - oneDayInMillis
+                //points.clear()
+                showMarkers()
+            }
+            "Last week" -> {
+                selectedTimeFrame = date - 7 * oneDayInMillis
+                //points.clear()
+                showMarkers()
+            }
+            "Last month" -> {
+                selectedTimeFrame = date - 30 * oneDayInMillis
+                //points.clear()
+                showMarkers()
+            }
+            else -> {
+                selectedTimeFrame = 0
+                //points.clear()
+                showMarkers()
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
     }
 
     override fun onResume() {
@@ -104,4 +154,6 @@ class MapsFragment : Fragment() {
 
         binding.mapView.onPause()
     }
+
+
 }

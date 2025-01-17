536745ae3789a601f9c8eab573a119028b85a1d6
MrDog210 <tusakf@gmail.com>
2025-01-17 21:57:35 +0100

    Added get blochchain REST function

--- app/lib/src/main/java/com/seljaki/lib/http.kt
+++ app/lib/src/main/java/com/seljaki/lib/http.kt
@@ -5,6 +5,7 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.forms.submitFormWithBinaryData
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.request.forms.formData
+import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
@@ -12,6 +13,7 @@ import io.ktor.http.HttpStatusCode
 import kotlinx.serialization.json.Json
 
 val SELJAKI_NET_SERVER = "https://seljakinet-server.schnapsen66.eu"
+val SELJAKI_CHAIN_SERVER = "http://smd.schnapsen66.eu:3336"
 
 val client = HttpClient(CIO)
 
@@ -34,6 +36,23 @@ suspend fun recognizeWeather(image: ByteArray, contentType: String): WeatherPred
 
         val body = response.bodyAsText()
         return Json.decodeFromString(body)
+    } catch (e: Exception) {
+        println(e)
+        null
+    }
+}
+
+suspend fun getBlockchain(): Blockchain? {
+    return try {
+        val response: HttpResponse = client
+            .get("$SELJAKI_CHAIN_SERVER/blockchain/blocks")
+
+        if (response.status != HttpStatusCode.OK)
+            return null
+
+        var body = response.bodyAsText()
+        body = body.replace("uuid", "UUID");
+        return Blockchain.fromJson(body)
     } catch (e: Exception) {
         println(e)
         null
